59d3d55df525a692a689bac1640fdd981d4235eb
Krystian Cybulski <krys@koliber.com>
2018-04-26 16:11:46 +0200

    Packaging for pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,31 @@
+from collections import OrderedDict
+
+from os import path
 from setuptools import find_packages, setup
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='django-goodgrids',
     description=(
-        'Create Excel files from CSVs using the GoodGrids API, based on an example Excel file template'
+        'Create Excel files from CSVs using the GoodGrids API, based on an example Excel file template.'
     ),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     version=__import__('django_goodgrids').__version__,
     author='Evertabs LLC',
     author_email='support@goodgrids.com',
     url='https://github.com/goodgrids/django-goodgrids',
+    project_urls=OrderedDict([
+        # ("Documentation", "https://django-goodgrids.readthedocs.io/en/%s/" % release_tag),
+        ("GoodGrids", "https://goodgrids.com"),
+        ("Source", "https://github.com/goodgrids/django-goodgrids"),
+        ("PyPi", "https://pypi.org/project/django-goodgrids"),
+        ("Tracker", "https://github.com/goodgrids/django-goodgrids/issues"),
+    ]),
     license='MIT License',
     packages=find_packages(),
     include_package_data=True,
